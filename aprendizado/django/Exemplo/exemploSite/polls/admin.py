9789855eca7593ee929bb033638d7143ffa5244a
Lucas de Oliveira Pereira <olucas1998@gmail.com>
2021-12-13 10:48:11 -0300

    Remoção da pasta aprendizado

--- aprendizado/django/Exemplo/exemploSite/polls/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Question
-
-# Register your models here.
-# aqui dizemos que o model Question tem uma interface Admin
-admin.site.register(Question)
